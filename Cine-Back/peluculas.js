a0dc6046e0a7f95521ffb9d684385a205df20da7
juanCriado <juanlchs182@gamil.com>
2024-11-18 23:06:47 -0300

    Agregar peliculas.js

---
+++ Cine-Back/peluculas.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send({ data });
+});
+
+router.post("/", (req, res) => {
+  res.send({ data });
+});
+
+router.delete("/:id", (req, res) => {
+  res.send({ data });
+});
+
+router.put("/:id", (req, res) => {
+  res.send({ data });
+});
+export default sumasRouter;
