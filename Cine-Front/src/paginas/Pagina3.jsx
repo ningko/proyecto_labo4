3348ef3c6e5a779e24b85ca664d9febe911fc35d
Martina Garcia <mgarcia.06@outlook.es>
2024-11-26 15:25:27 -0300

    Añadir overlay de horarios + placeholders

--- Cine-Front/src/paginas/Pagina3.jsx
+++ Cine-Front/src/paginas/Pagina3.jsx
@@ -4,8 +4,9 @@ import "./Pagina3.css";
 
 function App() {
     const [peliculas, setPeliculas] = useState([]);
-    const [titulo, setTitulo] = useState("");
-
+    const [peliculaSeleccionada, setPeliculaSeleccionada] = useState([])
+    const [overlayHorario, setOverlayHorario] = useState(false);
+    
     const getPeliculas = async () => {
         const response = await fetch("http://localhost:3000/peliculas/titulos");
         if (response.ok) {
@@ -18,19 +19,42 @@ function App() {
         getPeliculas();
       }, []);
 
+      const handleButtonClick = (pelicula) => {
+        setPeliculaSeleccionada(pelicula); 
+        setOverlayHorario(true);
+      };
+
+      const cerrarOverlay = () => {
+        setOverlayHorario(false); 
+        setPeliculaSeleccionada(null); 
+      };
+    
+
   return (
     <>
-    <h1>Peliculas</h1>
-    <ul>
+      <h1>Peliculas</h1>
+      <ul>
         {peliculas.map((pelicula) => (
-          <li key={pelicula.id}>
-            <a>
-            {`${pelicula.id}: ${pelicula.titulo}`}
-            </a>
+          <li key={pelicula.id_peliculas}>
+            <button onClick={() => handleButtonClick(pelicula)}>
+              {`${pelicula.titulo}`}
+            </button>
           </li>
         ))}
       </ul>
-    
+
+      {overlayHorario && (
+        <div className="overlay">
+          <div className="overlay-content">
+            <h3>{peliculaSeleccionada.titulo}</h3>
+            <button className="botones-horario">Viernes 29 de Noviembre - 18:00</button>
+            <button className="botones-horario">Viernes 29 de Noviembre - 21:00</button>
+            <button className="botones-horario">Viernes 29 de Noviembre - 23:30</button>
+            <br/>
+            <button className="boton-cerrar" onClick={cerrarOverlay}>x</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
